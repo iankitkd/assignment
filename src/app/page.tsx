2002964b7d88d90327fd11180df51b6089fb9056
Ankit Kumar <ankitkr.9105@gmail.com>
2025-06-06 17:22:50 +0530

    Add home page buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex items-center justify-center p-4 gap-4">
+      <Link href="/signup" className="rounded-full bg-gradient-to-r from-left-color to-right-color text-white px-4 py-2">Sign up</Link>
+      <Link href="/dashboard" className="rounded-full border border-left-color px-4 py-2">Dashboard</Link>
     </div>
   );
 }
